91addac2d69875e4713dbf6b2d0c2b17185a48ca
musca <muscaestar@hotmail.com>
2020-07-10 02:17:40 +1000

    import project from spring init

---
+++ src/main/java/xyz/muscaestar/muscacafebarista/MuscaCafeBaristaApplication.java
@@ -0,0 +1,13 @@
+package xyz.muscaestar.muscacafebarista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuscaCafeBaristaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuscaCafeBaristaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/muscaestar/muscacafebarista/MuscaCafeBaristaApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.muscaestar.muscacafebarista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuscaCafeBaristaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
